49db6a543e4388c0d4396018d67c4d7895176d71
郑泽涛 <1050713479@qq.com>
2025-02-18 14:44:15 +0800

    2.9.13

--- ws_restart_stop/app.js
+++ ws_restart_stop/app.js
@@ -1,7 +1,7 @@
 const WebSocket = require('ws');
 
 // Home Assistant 配置
-const wsUrl = process.env.SUPERVISOR_WS_URL || 'ws://supervisor/api/websocket';
+const wsUrl = process.env.SUPERVISOR_WS_URL || 'ws://supervisor/core/websocket';
 const haToken = process.env.SUPERVISOR_TOKEN;
 
 console.log(`wsUrl = `, wsUrl);
