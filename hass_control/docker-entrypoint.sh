9f5ac98002e064c2d9a91bc8e26448d4e71c8b22
zetao.zheng <1050713479@qq.com>
2025-04-07 12:01:04 +0800

    输出mqtt配置

--- hass_control/docker-entrypoint.sh
+++ hass_control/docker-entrypoint.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bashio
+bashio::log.info "Preparing to start..."
 
 MQTT_HOST=$(bashio::services mqtt "host")
 MQTT_PORT=$(bashio::services mqtt "port")
 MQTT_USER=$(bashio::services mqtt "username")
 MQTT_PASS=$(bashio::services mqtt "password")
+
+bashio::log.info "Host: ${MQTT_HOST}"
+bashio::log.info "Port: ${MQTT_PORT}"
+bashio::log.info "Username: ${MQTT_USER}"
+bashio::log.info "Password: ${MQTT_PASS}"
 
 export MQTT_HOST MQTT_PORT MQTT_USER MQTT_PASS
 
