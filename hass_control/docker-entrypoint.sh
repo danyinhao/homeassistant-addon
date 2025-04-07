d353dc364d9d520204e4c7c984fb5ead5ba3342f
zetao.zheng <1050713479@qq.com>
2025-04-07 11:29:01 +0800

    1

---
+++ hass_control/docker-entrypoint.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bashio
+
+MQTT_HOST=$(bashio::services mqtt "host")
+MQTT_PORT=$(bashio::services mqtt "port")
+MQTT_USER=$(bashio::services mqtt "username")
+MQTT_PASS=$(bashio::services mqtt "password")
+
+export MQTT_HOST MQTT_PORT MQTT_USER MQTT_PASS
+
+exec "$@"
