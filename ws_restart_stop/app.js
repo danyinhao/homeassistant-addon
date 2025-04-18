b61cf8497970e97a2a546509fa18f54df6a623ed
zetao.zheng <1050713479@qq.com>
2025-04-18 15:43:44 +0800

    1

--- ws_restart_stop/app.js
+++ ws_restart_stop/app.js
@@ -12,146 +12,146 @@ const haToken = process.env.SUPERVISOR_TOKEN;
 
 const SUPERVISOR_URL = "http://supervisor";
 
-console.log(`wsUrl = `, wsUrl);
-console.log(`haToken = `, haToken);
+// console.log(`wsUrl = `, wsUrl);
+// console.log(`haToken = `, haToken);
 
-// 创建 WebSocket 连接
-const ws = new WebSocket(wsUrl, {
-  autoPong: true,
-  headers: {
-    Authorization: `Bearer ${haToken}`,
-    "Content-Type": "application/json",
-  }
-});
+// // 创建 WebSocket 连接
+// const ws = new WebSocket(wsUrl, {
+//   autoPong: true,
+//   headers: {
+//     Authorization: `Bearer ${haToken}`,
+//     "Content-Type": "application/json",
+//   }
+// });
 
-// 监听 WebSocket 连接打开事件
-ws.on('open', () => {
-  console.log('WebSocket connection opened');
-});
+// // 监听 WebSocket 连接打开事件
+// ws.on('open', () => {
+//   console.log('WebSocket connection opened');
+// });
 
-// 监听 WebSocket 消息事件
-ws.on('message', (data) => {
-  // console.log(`message1 =>`, data);s'sss
+// // 监听 WebSocket 消息事件
+// ws.on('message', (data) => {
+//   // console.log(`message1 =>`, data);s'sss
 
-  console.log(`message =>`, data.toString());
-  const message = JSON.parse(data);
-  if (message.type === 'event') {
-    const event = message.event;
+//   console.log(`message =>`, data.toString());
+//   const message = JSON.parse(data);
+//   if (message.type === 'event') {
+//     const event = message.event;
 
-    if (event.event_type === 'homeassistant_stop') {
-      console.log('Home Assistant is stopping...');
-      // 在这里执行关机逻辑
-    } else if (event.event_type === 'homeassistant_start') {
-      console.log('Home Assistant is starting...');
-      // 在这里执行启动逻辑
-    }
-  } else if (message.type === 'auth_required') {
-    console.log('Authentication required');
-    // 发送认证消息
-    ws.send(
-      JSON.stringify({
-        type: 'auth',
-        access_token: haToken,
-      })
-    );
-  } else if (message.type === 'auth_ok') {
-    console.log('Authentication successful');
-    const id = Math.floor(Math.random() * 900000) + 100000;
-    // 订阅事件
-    // ws.send(
-    //   JSON.stringify({
-    //     id, // 请求 ID
-    //     type: 'subscribe_trigger',
-    //     // event_type: 'shutdown', // 监听的事件类型
-    //     trigger: {
-    //       "platform": "homeassistant",
-    //       "event": "shutdown"
-    //     }
-    //   })
-    // );
+//     if (event.event_type === 'homeassistant_stop') {
+//       console.log('Home Assistant is stopping...');
+//       // 在这里执行关机逻辑
+//     } else if (event.event_type === 'homeassistant_start') {
+//       console.log('Home Assistant is starting...');
+//       // 在这里执行启动逻辑
+//     }
+//   } else if (message.type === 'auth_required') {
+//     console.log('Authentication required');
+//     // 发送认证消息
+//     ws.send(
+//       JSON.stringify({
+//         type: 'auth',
+//         access_token: haToken,
+//       })
+//     );
+//   } else if (message.type === 'auth_ok') {
+//     console.log('Authentication successful');
+//     const id = Math.floor(Math.random() * 900000) + 100000;
+//     // 订阅事件
+//     // ws.send(
+//     //   JSON.stringify({
+//     //     id, // 请求 ID
+//     //     type: 'subscribe_trigger',
+//     //     // event_type: 'shutdown', // 监听的事件类型
+//     //     trigger: {
+//     //       "platform": "homeassistant",
+//     //       "event": "shutdown"
+//     //     }
+//     //   })
+//     // );
 
-    // ws.send(
-    //   JSON.stringify({
-    //     id: id + 1, // 请求 ID
-    //     type: 'subscribe_events',
-    //     event_type: 'EVENT_HOMEASSISTANT_STOP', // 监听的事件类型
-    //   })
-    // );
+//     // ws.send(
+//     //   JSON.stringify({
+//     //     id: id + 1, // 请求 ID
+//     //     type: 'subscribe_events',
+//     //     event_type: 'EVENT_HOMEASSISTANT_STOP', // 监听的事件类型
+//     //   })
+//     // );
 
-    // ws.send(
-    //   JSON.stringify({
-    //     id: id + 2, // 请求 ID
-    //     type: 'subscribe_events',
-    //     event_type: 'homeassistant_started', // 监听的事件类型
-    //   })
-    // );
+//     // ws.send(
+//     //   JSON.stringify({
+//     //     id: id + 2, // 请求 ID
+//     //     type: 'subscribe_events',
+//     //     event_type: 'homeassistant_started', // 监听的事件类型
+//     //   })
+//     // );
 
-    // ws.send(
-    //   JSON.stringify({
-    //     id: id + 3, // 请求 ID
-    //     type: 'subscribe_events',
-    //     event_type: 'homeassistant_start', // 监听的事件类型
-    //   })
-    // );
+//     // ws.send(
+//     //   JSON.stringify({
+//     //     id: id + 3, // 请求 ID
+//     //     type: 'subscribe_events',
+//     //     event_type: 'homeassistant_start', // 监听的事件类型
+//     //   })
+//     // );
 
-    // ws.send(
-    //   JSON.stringify({
-    //     id: id + 4, // 请求 ID
-    //     type: 'subscribe_events',
-    //     event_type: 'homeassistant_stop', // 监听的事件类型
-    //   })
-    // );
+//     // ws.send(
+//     //   JSON.stringify({
+//     //     id: id + 4, // 请求 ID
+//     //     type: 'subscribe_events',
+//     //     event_type: 'homeassistant_stop', // 监听的事件类型
+//     //   })
+//     // );
 
-    // ws.send(
-    //   JSON.stringify({
-    //     id: id + 5, // 请求 ID
-    //     type: 'subscribe_events',
-    //     event_type: 'homeassistant_final_write', // 监听的事件类型
-    //   })
-    // );
+//     // ws.send(
+//     //   JSON.stringify({
+//     //     id: id + 5, // 请求 ID
+//     //     type: 'subscribe_events',
+//     //     event_type: 'homeassistant_final_write', // 监听的事件类型
+//     //   })
+//     // );
 
-    // ws.send(
-    //   JSON.stringify({
-    //     id: id + 6, // 请求 ID
-    //     type: 'subscribe_events',
-    //     event_type: 'homeassistant_close', // 监听的事件类型
-    //   })
-    // );
+//     // ws.send(
+//     //   JSON.stringify({
+//     //     id: id + 6, // 请求 ID
+//     //     type: 'subscribe_events',
+//     //     event_type: 'homeassistant_close', // 监听的事件类型
+//     //   })
+//     // );
 
-    setInterval(() => {
-      ws.ping();
-    }, 5000);
+//     setInterval(() => {
+//       ws.ping();
+//     }, 5000);
 
-    // ws.send(
-    //     JSON.stringify({
-    // id: 2, // 请求 ID
-    // type: 'subscribe_events',
-    // event_type: 'homeassistant_start', // 监听的事件类型
-    //     })
-    // );
+//     // ws.send(
+//     //     JSON.stringify({
+//     // id: 2, // 请求 ID
+//     // type: 'subscribe_events',
+//     // event_type: 'homeassistant_start', // 监听的事件类型
+//     //     })
+//     // );
 
-  } else {
-    console.log('Received message:', message);
-  }
-});
+//   } else {
+//     console.log('Received message:', message);
+//   }
+// });
 
-// 监听 WebSocket 错误事件
-ws.on('error', (error) => {
-  console.error('WebSocket error:', error);
-});
+// // 监听 WebSocket 错误事件
+// ws.on('error', (error) => {
+//   console.error('WebSocket error:', error);
+// });
 
-// 监听 WebSocket 关闭事件
-ws.on('close', () => {
-  console.log('WebSocket connection closed');
-});
+// // 监听 WebSocket 关闭事件
+// ws.on('close', () => {
+//   console.log('WebSocket connection closed');
+// });
 
 process.on("SIGINT", async (singals) => {
-  await getSuperviorInfo();
+  // await getSuperviorInfo();
   console.log(` SIGINT singals = `, singals);
   console.log(`date now = `, Date.now());
 });
 process.on("SIGTERM", async (singals) => {
-  await getSuperviorInfo();
+  // await getSuperviorInfo();
   console.log(` SIGTERM singals = `, singals);
   console.log(`date now = `, Date.now());
 });
@@ -228,7 +228,7 @@ async function getSuperviorInfo() {
   // }
   try {
     const response = await axios.get(`${SUPERVISOR_URL}/core/api/config`, { headers });
-    console.log(`/services entities = `, JSON.stringify(response.data));
+    console.log(`/config config = `, JSON.stringify(response.data));
   } catch (e) {
     console.log(e);
   }
